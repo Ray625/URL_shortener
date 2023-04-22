fed10e24769417a376a424791fa9b05d2c23a97d
Ray625 <wdvgy32@gmail.com>
2023-04-23 01:42:41 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const app = express()
 
 app.engine('hbs', exphbs({ defaultlayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+
+// 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.render('index')
